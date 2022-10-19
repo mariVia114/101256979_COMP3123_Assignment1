81474766dd5ad3769078d18f124f5100ada10d42
Marie <marievianca.pagaduan@georgebrown.ca>
2022-10-19 02:55:26 -0400

    readme file added,encryption on password added

---
+++ README.md
@@ -0,0 +1,2 @@
+## COMP 3123 - INDIVIDUAL ASSIGNMENT ONE
+As a newly hired Jr. Software Engineer my manager assigned me a task to develop Backend application using NodeJS, Express and MongoDB.
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,43 +1,39 @@
 const express = require("express")
+const bcrypt = require("bcrypt");
 const routes = express();
+
 const usersModel = require('../models/usersModel.js');
+routes.post("/signup", async (req, res) => {
+    const body = req.body;
 
-
-//TODO - Create a new user - /api/user/signup
-routes.post('/signup', async (req, res) => {
-    // Validate request
-    try{
-        const newUser = new usersModel(req.body)
-        const user = await newUser.save()
-        res.status(201).send(user) 
-    }catch(error){
-        res.status(400).send(error)
+    if (!(body.email && body.password)) {
+      return res.status(400).send({ error: "Data not formatted properly" });
     }
-});
 
-//TODO - Login - api/user/login
-routes.post('/login', async (req, res) => {
-    /*
-    if(username == authorisedUser && password == savedPwd){
-    res.json({status:true, message:"User Is valid"});
-  }else if(username != authorisedUser){
-    res.json({status:false, message:"User is invalid"});
-  }else if(password != savedPwd){
-    res.json({status:false, message:"Password is invalid"});
-  }
-     */
-    try{
-        // const {username, password} =  req.body
-        const authorisedUser = await usersModel.findOne(req.body)
-        if(authorisedUser)
-        {
-                res.status(200).json({status:true,username:authorisedUser.username,message:"User logged in succesfully!"});
-        }else{
+    // creating a new mongoose doc from user data
+    const user = new usersModel(body);
+    // generate salt to hash password
+    const salt = await bcrypt.genSalt(10);
+    // now we set user password to hashed password
+    user.password = await bcrypt.hash(user.password, salt);
+    user.save().then((doc) => res.status(201).send(doc));
+  });
+
+  // login route
+  routes.post("/login", async (req, res) => {
+    const body = req.body;
+    const user = await usersModel.findOne({ username: body.username });
+    if (user) {
+      // check user password with hashed password stored in the database
+      const validPassword = await bcrypt.compare(body.password, user.password);
+      if (validPassword) {
+            res.status(200).json({status:true,username:user.username,message:"User logged in succesfully!"});
+      } else {
             res.status(200).json({status:false,message:"Invalid Username and password"});
-        }
-    }catch(error){
-        res.status(400).send(error)
+      }
+    } else {
+      res.status(400).json({ error: "User does not exist" });
     }
-});
+  });
 
 module.exports = routes;
